42a37d592af8635f780c303d5510b3d0a43eafc5
alff0x1f <alff0x1f@gmail.com>
2019-08-20 16:50:34 +0500

    add ex1.3

--- ch1/ex1.3/ex1.3.go
+++ ch1/ex1.3/ex1.3.go
@@ -5,6 +5,25 @@ write benchmark tests for systematic perfomance evaluation)
 */
 package main
 
+import (
+	"fmt"
+	"os"
+	"strings"
+	"time"
+)
+
 func main() {
-	//!TODO
+	start := time.Now()
+	s, sep := "", ""
+	for _, arg := range os.Args[1:] {
+		s += sep + arg
+		sep = " "
+	}
+	fmt.Println(s)
+	secs1 := time.Since(start).Seconds()
+
+	start = time.Now()
+	fmt.Println(strings.Join(os.Args[1:], " "))
+	secs2 := time.Since(start).Seconds()
+	fmt.Printf("Time uneficient: %f, time by join func: %f, diff %.1f times\n", secs1, secs2, secs1/secs2)
 }
